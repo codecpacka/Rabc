2cb3ef47c24548153b23e0e1b04947bab6bce06a
codecpacka <ramprasad.r2600@gmail.com>
2022-04-28 09:53:26 +0530

    basic user layout ready

--- routes/user.route.js
+++ routes/user.route.js
@@ -5,5 +5,10 @@ router.get("/profile", async (req, res, next) => {
   const person = req.user
   res.render("profile", { person })
 })
+router.get("/dashboard", async (req, res, next) => {
+  console.log(req.user)
+  const person = req.user
+  res.render("user")
+})
 
 module.exports = router
